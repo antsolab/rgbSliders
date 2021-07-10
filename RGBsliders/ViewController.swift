b3057f6154a30b9963b9f317bb83e5faf909d24e
antsolab <antsolab@protonmail.com>
2021-07-10 23:18:53 +0300

    work with logic

--- RGBsliders/ViewController.swift
+++ RGBsliders/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redGradationLabel: UILabel!
+    @IBOutlet weak var greenGradationLabel: UILabel!
+    @IBOutlet weak var blueGradationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        gradiationLabels()
+        colorView.layer.cornerRadius = 10
+        changeColor()
     }
-
+    
+    func gradiationLabels() {
+        redGradationLabel.text = String(redSlider.value)
+        greenGradationLabel.text = String(greenSlider.value)
+        blueGradationLabel.text = String(blueSlider.value)
+    }
+    
+    func changeColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
+    
+    
+    
+    @IBAction func rgbView() {
+        changeColor()
+        gradiationLabels()
+    }
+    
 
 }
 
