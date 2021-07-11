d4bc37054539692be0534c306f5b6fc7ca534cd0
antsolab <antsolab@protonmail.com>
2021-07-11 21:21:32 +0300

    some fixes

--- RGBsliders/ViewController.swift
+++ RGBsliders/ViewController.swift
@@ -21,17 +21,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         gradiationLabels()
-        colorView.layer.cornerRadius = 10
         changeColor()
+        
+        colorView.layer.cornerRadius = 10
+
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
     }
     
-    func gradiationLabels() {
-        redGradationLabel.text = String(round(redSlider.value * 100) / 100)
-        greenGradationLabel.text = String(round(greenSlider.value * 100) / 100)
-        blueGradationLabel.text = String(round(blueSlider.value * 100) / 100)
+    private func gradiationLabels() {
+        redGradationLabel.text = String(format: "%.2f", redSlider.value)
+        greenGradationLabel.text = String(format: "%.2f", greenSlider.value)
+        blueGradationLabel.text = String(format: "%.2f", blueSlider.value)
     }
     
-    func changeColor() {
+    private func changeColor() {
         colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
                                             green: CGFloat(greenSlider.value),
                                             blue: CGFloat(blueSlider.value),
